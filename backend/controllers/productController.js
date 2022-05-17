54a70f3d851d334fba52055dab9dbf99be2d9d93
KetanMangidkar <ketanmangidkar@gmail.com>
2022-05-17 11:58:41 +0530

    product update,delete,create,details

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,3 +25,79 @@ exports.getAllProducts = async (req,res,next)=>{
     }
      
 }
+//get single product
+
+
+
+exports.getProductDetails = async (req,res,next)=>{
+  try {
+      let product = await Product.findById(req.params.id);
+      if(!product){
+        return res.status(500).json({
+          sucess:false,
+          message:"Product not found"
+        })
+      }
+      res.status(200).json({
+        success:true,
+        product
+      })
+
+  } catch (error) {
+    res.status(401).json({message: err})
+  }
+}
+
+//update product admin 
+
+exports.updateProduct = async (req,res,next) =>{
+ 
+    
+ 
+  try {
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+      return res.status(500).json({
+        sucess:false,
+        message:"Product not found"
+      })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators:true,useFindAndModify:false })
+
+    res.status(200).json({
+      success:true,
+      product
+    })
+    
+  } catch (err) {
+    res.status(401).json({message: err})
+  }
+  
+}
+
+ //delete product --admin
+exports.deleteProduct = async (req,res,next)=>{
+  try {
+      let product = await Product.findById(req.params.id);
+      if(!product){
+        return res.status(500).json({
+          sucess:false,
+          message:"Product not found"
+        })
+      }
+
+      await product.remove();
+      res.status(200).json({
+        success:true,
+        message:"product successfully deleted"
+      })
+
+  } catch (error) {
+    res.status(401).json({message: err})
+  }
+}
+
+
+
+
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
-const { getAllProducts,createProduct } = require("../controllers/productController");
+const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails } = 
+require("../controllers/productController");
 
 const router = express.Router();
 
@@ -8,6 +9,9 @@ const router = express.Router();
 router.route("/products").get(getAllProducts);
 router.route("/products/new").post(createProduct);
 
-router.route("/product/:id").put()
+router.route("/product/:id").put(updateProduct);
+
+router.route("/product/:id").delete(deleteProduct);
+router.route("/product/:id").get(getProductDetails);
 
 module.exports = router;
